c5359615b3706dfa4df63c931ee0dce971215073
Raffaele Pizzari <raffaele.pizzari@gmail.com>
2020-03-31 13:10:01 +0200

    Aggiungo lo script SFTP e la libreria relativa

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -1,107 +1,82 @@
 // deploy.js
 const fs = require('fs');
-const Client = require('ssh2-sftp-client');
+const sftp = require('ssh2-sftp-client');
 const glob = require('glob');
 
 const basePath = './dist';
-const destinationPath = '/httpdocs/sos';
-const config = {
-  // We store the credentials for
-  // our SFTP server as environemnt
-  // variables for security reasons.
-  host: process.env.SFTPHOST,
-  port: process.env.SFTPPORT,
-  password: process.env.SFTPPASS,
-  user: process.env.SFTPUSERNAME,
+const destinationPath = '/upload';
+
+const localConfig = {
+  host: null,
+  username: null,
+  password: null,
+  port: null,
 };
-const sftpClient = new Client();
 
-console.log("ci provo");
+const IS_LOG_ENABLED = true;
+const LOG_SUCCESS = true;
+const LOG_FAILED = true;
 
-sftpClient.connect(config);
+const localLog = (isSuccess, log) => {
+  if(!IS_LOG_ENABLED) return false;
 
-sftpClient.on('ready', () => {
-  console.log("connesso!");
-  // Get an array of all files and directories
-  // in the given base path and send them to the
-  // `handlePath()` function to decide if a
-  // directory is created on the server or the
-  // file is uploaded.
-  glob.sync(`${basePath}/**/*`).forEach(handlePath);
-  //cleanupRemoteDirectory(destinationPath);
-});
-
-function createDirectory(destination) {
-  return sftpClient.mkdir(destination, true, (error) => {
-    if (error) throw error;
-
-    sftpClient.end();
-  });
-}
-
-function uploadFile(file, destination) {
+  if(LOG_SUCCESS && isSuccess) {
+    console.log(`SUCCESS => ${log}`);
+  }
   
-  sftpClient.connect(config);
-  sftpClient.put(file, destination, (error) => {
-    if (error) throw error;
+  if(LOG_FAILED && !isSuccess) {
+    console.log(`FAILED => ${log}`);
+  }
+} 
+const client = new sftp('test');
 
-    console.log(`${file} => ${destination}`);
-    sftpClient.end();
-  });
+async function createDirectory(destination) {
+  try {
+    localLog(true, `mkdir: ${destination}`);
+    await client.mkdir(destination, true);
+    localLog(true, `mkdir: ${destination}`);
+    return true;
+  } catch(err) {
+    localLog(false, `mkdir: ${destination}`);
+    localLog(false, err);
+    return false;
+  }
 }
 
-// Check if the path is a directory and
-// either create the directory on the server
-// if it is a directory, or upload the file
-// if it is a file.
-function handlePath(path) {
+async function uploadFile(file, destination) {
+  try {
+    localLog(true, `fastPut: ${destination}`);
+    await client.fastPut(file, destination);
+    localLog(true, `fastPut: ${file} => ${destination}`);
+    return true;
+  } catch(err) {
+    localLog(false, `fastPut: ${file} => ${destination}`);
+    localLog(false, err);
+    return false;
+  }
+}
+
+async function handlePath(path) {
   const relativeFile = path.replace(`${basePath}/`, '');
-  const destination = `${destinationPath}/${relativeFile}`;
-
+  let destination = null;
+  try {
+    destination = await client.realPath(`${destinationPath}/${relativeFile}`);
+  } catch(err) {
+    localLog(`realPath: ${destinationPath}/${relativeFile}: FAILED!`);
+  }
   if (fs.lstatSync(path).isDirectory()) {
-    return createDirectory(destination);
+    return destination ? await createDirectory(destination) : false;
   }
-
-  return uploadFile(path, destination);
+  return destination ? await uploadFile(path, destination) : false;
 }
 
-const EXPIRATION_DATE_IN_DAYS = 7;
-
-function isExpired(date) {
-  const oneDayInMilliseconds = 86400000;
-  const timestamp = new Date(date).getTime();
-  const expirationTimestamp = Date.now() - (oneDayInMilliseconds * EXPIRATION_DATE_IN_DAYS);
-
-  return timestamp < expirationTimestamp;
-}
-
-function cleanup(pathObject, directory) {
-  if (pathObject.name === '.' || pathObject.name === '..') return;
-
-  const path = `${directory}/${pathObject.name}`;
-
-  // If the current path is a directory
-  // we recursively check the files in it.
-  if (pathObject.type === 'd') {
-    return cleanupRemoteDirectory(path);
+(async function() {
+  await client.connect(localConfig);
+  localLog(true, `connessione a ${localConfig.host}`);
+  const paths = glob.sync(`${basePath}/**/*`);
+  for(let path of paths) {
+    await handlePath(path);
   }
-
-  if (isExpired(pathObject.date)) {
-    sftpClient.delete(path, (error) => {
-      if (error) throw error;
-
-      console.log(`Removed: ${path}`);
-      sftpClient.end();
-    });
-  }
-}
-
-function cleanupRemoteDirectory(directory) {
-  return sftpClient.list(directory, (error, pathObjects) => {
-    if (error) throw error;
-
-    pathObjects.forEach(pathObject => cleanup(pathObject, directory));
-    sftpClient.end();
-  });
-}
-
+  localLog(true, `chiusura connessione a ${localConfig.host}`);
+  await client.end();
+})();
